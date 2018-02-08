ef9653d7eecaa6aba585711ab0390a0c0b0ae243
Rafael Souza <rafa.jones@hotmail.com>
2018-02-08 14:28:42 -0200

    Mudanças

---
+++ js/newCalendar.js
@@ -0,0 +1,21 @@
+var objeto = [];
+
+
+var evento = ({
+    nome: null,
+    descricao: null,
+    dataI: null,
+    dataT: null,
+    categoria: null,
+    investimento: null,
+    site: null,
+    cidade: null
+});
+
+function salvarObjeto() {
+    
+}
+
+function inserirObjetoNoPlano() {
+    
+}
